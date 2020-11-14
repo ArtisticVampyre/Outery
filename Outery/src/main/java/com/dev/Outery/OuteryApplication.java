da2c64d8be61b14af345a2ac745eab700dfafd54
sergiu0209 <solovastru.sergiu.0209@outlook.com>
2020-11-14 10:32:53 +0200

    no message

---
+++ Outery/src/main/java/com/dev/Outery/OuteryApplication.java
@@ -0,0 +1,13 @@
+package com.dev.Outery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OuteryApplication.class, args);
+	}
+
+}
---
+++ Outery/src/test/java/com/dev/Outery/OuteryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.Outery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuteryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
